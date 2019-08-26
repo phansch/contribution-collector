39cce63ce662553a894425a5fa668ff84c40b793
Philipp Hansch <dev@phansch.net>
2019-08-26 21:03:59 +0200

    Structurize lib.rs
    No more println! in there =)
    
    Slowly moving towards a point where I can easily print JSON.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    prs::fetch().unwrap();
+    let prs = prs::fetch().unwrap();
+
+    for pr in prs {
+        let text = format!(
+            "Title:     {:?}\n\
+            Body:      {:?}\n\
+            HTML URL:  {:?}\n\
+            State:     {:?}\n\
+            Closed at: {:?}\n\
+            ---------\n",
+            pr.title, pr.body, pr.html_url,
+            pr.state, pr.closed_at
+        );
+        println!("{}", text);
+    }
 }
